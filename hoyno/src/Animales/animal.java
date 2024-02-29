5363a4009da417ae121ea0f5ac448b7d5f1365ec
rubences <91731294+rubences@users.noreply.github.com>
2024-02-29 10:31:00 +0000

    animales

---
+++ hoyno/src/Animales/animal.java
@@ -0,0 +1,5 @@
+package Animales;
+
+public class animal {
+    
+}
---
+++ hoyno/src/Animales/animal_aviario.java
@@ -0,0 +1,19 @@
+public class animal_aviario extends animal {
+    public animal_aviario() {
+    }
+    public String toString() {
+        return "animal_aviario []";
+    }
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
