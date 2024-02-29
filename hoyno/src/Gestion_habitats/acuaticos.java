87193cc30bccaa50eb584be4f30c1ad7a8ce4333
rubences <91731294+rubences@users.noreply.github.com>
2024-02-29 09:58:27 +0000

    creando acuarios

---
+++ hoyno/src/Gestion_habitats/acuaticos.java
@@ -0,0 +1,38 @@
+package Gestion_habitats;
+
+public class acuaticos extends habitats{
+
+    public acuaticos() {
+    }
+
+
+    @Override
+    public String toString() {
+        return "acuaticos []";
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        // TODO Auto-generated method stub
+        return super.clone();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // TODO Auto-generated method stub
+        return super.equals(obj);
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        // TODO Auto-generated method stub
+        super.finalize();
+    }
+
+    @Override
+    public int hashCode() {
+        // TODO Auto-generated method stub
+        return super.hashCode();
+    }
+    
+}
