17b68fffb6494baf7acb331c2f6c96658a88837d
rubences <91731294+rubences@users.noreply.github.com>
2024-02-29 10:04:49 +0000

    mas cambios

---
+++ hoyno/src/Gestion_habitats/aviario.java
@@ -0,0 +1,21 @@
+package Gestion_habitats;
+
+public class aviario extends Gestion_habitats.habitats {
+    public aviario() {
+    }
+    public String toString() {
+        return "aviario []";
+    }
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
+    }
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+    protected void finalize() throws Throwable {
+        super.finalize();
+    }
+    public int hashCode() {
+        return super.hashCode();
+    }
+}
---
+++ hoyno/src/Gestion_habitats/terrestres.java
@@ -0,0 +1,37 @@
+package Gestion_habitats;
+
+public class terrestres extends habitats    {
+
+    public terrestres() {
+    }
+
+    @Override
+    public String toString() {
+        return "terrestres []";
+    }
+
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        // TODO Auto-generated method stub
+        return super.clone();
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        // TODO Auto-generated method stub
+        return super.equals(obj);
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        // TODO Auto-generated method stub
+        super.finalize();
+    }
+
+    @Override
+    public int hashCode() {
+        // TODO Auto-generated method stub
+        return super.hashCode();
+    }
+    
+}
